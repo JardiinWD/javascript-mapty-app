272f2968d14b6b0fe4829bb4b673235db43c9bf3
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-09-26 09:25:35 +0200

    #2- Fixed markup with comments, added stylesheet and script regions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+//#region Const and Variables 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -10,3 +8,11 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+//#endregion
+
+//#region Data
+
+// Months
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+//#endregion
